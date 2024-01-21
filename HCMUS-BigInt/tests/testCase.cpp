f1a8e7b9eb2085bcfb2106074dfa00fb86fdf50c
PhucVR <59957741+nguyenphuc22@users.noreply.github.com>
2024-01-21 17:32:19 +0700

    Fix incorrect exit code in testCase.cpp

--- HCMUS-BigInt/tests/testCase.cpp
+++ HCMUS-BigInt/tests/testCase.cpp
@@ -116,7 +116,7 @@ int main(int argc, char* argv[]) {
         } else {
             std::cout << "Incorrect" << std::endl;
             inputFile.close();
-            exit(1);
+            return 1;
         }
                
     }
