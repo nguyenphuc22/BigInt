982e93264a52bbe8e7f6e60973b8aef03476e0d5
lightning <ntphong0094@gmail.com>
2024-03-04 23:11:34 +0700

    First upload

--- CreateTestCaseBigInt/main.py
+++ CreateTestCaseBigInt/main.py
@@ -38,10 +38,7 @@ def randomBigInt(operation, range=10**64):
     if operation == Pow:
         return random.randint(2, 10**3)  # Adjust this range as needed for Pow operation
     else:
-        if operation == Mod:
-            return random.randint(1, range)
-        else:
-            return random.randint(-range, range)
+        return random.randint(-range, range)
 
 def getFileName(operation):
     if operation == Add:
@@ -92,15 +89,7 @@ def performOperation(operation, num1, num2):
     elif operation == Mul:
         result = num1 * num2
     elif operation == Div:
-        from decimal import Decimal, getcontext
-
-        getcontext().prec = 100  # Set the precision to 100
-
-        num1 = Decimal(str(num1))
-        num2 = Decimal(str(num2))
-
         result = num1 / num2
-
     elif operation == Mod:
         result = num1 % num2
     elif operation == Pow:
@@ -188,3 +177,15 @@ if __name__ == '__main__':
     createFileTestCaseBigIntCompare(1000,LargerThanEqual)
     createFileTestCaseBigIntCompare(1000,other)
     print_hi('PyCharm')
+    
+
+
+
+
+# ./HCMUS-BigInt-Test ../../CreateTestCaseBigInt/data/sub.txt
+# ./HCMUS-BigInt ../../CreateTestCaseBigInt/data/sub.txt
+# ./HCMUS-BigInt-Test /home/nguyenthanhphong/HCMUS-BigInt-main/CreateTestCaseBigInt/data/sub.txt
+
+
+
+
---
+++ HCMUS-BigInt/build/CMakeFiles/HCMUS-BigInt.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for HCMUS-BigInt.
+# This may be replaced when dependencies are built.
